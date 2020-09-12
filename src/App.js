a203a8adcf24b7c300be9b68dd9d30590629166c
Stefan Vrecic <vstefan@hotmail.co.uk>
2020-09-13 00:40:19 +1200

    comments

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import { Route, Redirect } from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
 
 /*
+I did manage to get all of the features however:
+
 Instead of creating seperate components for Modal A/B and Modal C, 
 I used one Modal which renders different content depending on the Modal selected. 
 Upon further reflection, it seems you expected me to use Redux to pass the state via a store between Modals.
 
+I completely misinterpreted what you meant by 'think about it as part of something bigger' -- thought
+you meant bigger opportunity, but obviously you meant a bigger app/scale. Very foolish of me!
 
 I didn't consider how this app would work on scale, so that was definitely a shortcoming on my implementation.
+
  For example I may have structured the files or classes differently, or seperated the folders. I would also
- add a config file for the 
+ add a config file for the token...
 
 Unfortunately due to personal time restraints within the 36 hours,
 I do not have enough time to restructure this app within your expectations. 
